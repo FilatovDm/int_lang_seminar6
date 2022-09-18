034618a6e1a29f718092cbc43661202161dc4b90
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-18 10:37:21 +0300

    Example 42 completed

---
+++ Example42/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать 
+десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+string GetBinaryNumber(int number)
+{
+    string result = String.Empty;
+    while (number > 0)
+    {
+        result = Convert.ToString(number % 2) + result;
+        number = number / 2;
+    }
+    return result;
+}
+
+Console.Write("Введите десятичное число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+string binaryNumber = GetBinaryNumber(decimalNumber);
+Console.Write($"Десятичное число {decimalNumber} в двоичном формате: {binaryNumber}");
