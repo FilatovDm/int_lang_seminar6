f99c9bc5a7923d7eb63414d258b5e84bf4edc4d6
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-18 12:13:16 +0300

    Example 45 completed

---
+++ Example45/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ Задача 45: Напишите программу, которая будет создавать копию 
+ заданного массива с помощью поэлементного копирования.
+*/
+
+int[] GetCopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+
+    for (int i = 0; i < copyArray.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+int[] inputArray = new int[] {23, -5, 6, 0, 69};
+int[] copyArray = GetCopyArray(inputArray);
+
+System.Console.WriteLine("[{0}]", string.Join(", ", copyArray));
